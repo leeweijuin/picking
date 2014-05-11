2b48cc4e1dcd3ef546ec787b36a82a9176c419e6
Poh Guan <ngpohguan@gmail.com>
2014-05-11 20:02:21 +0800

    Added FB login button logic

---
+++ PickIt/src/invention/pickit/LoginActivity.java
@@ -0,0 +1,26 @@
+package invention.pickit;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class LoginActivity extends FragmentActivity {
+	private LoginFragment loginFragment;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+
+	    if (savedInstanceState == null) {
+	        // Add the fragment on initial activity setup
+	        loginFragment = new LoginFragment();
+	        getSupportFragmentManager()
+	        .beginTransaction()
+	        .add(android.R.id.content, loginFragment)
+	        .commit();
+	    } else {
+	        // Or set the fragment from restored state info
+	        loginFragment = (LoginFragment) getSupportFragmentManager()
+	        .findFragmentById(android.R.id.content);
+	    }
+	}
+}
